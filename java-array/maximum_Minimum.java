7f0156bf36f5518cc85d7dec3c5dc3e90e3eed9f
kartik1208-cyber <pagarkartik2@gmail.com>
2026-09-14 19:06:00 +0530

    we found maximum and minimum vlaues in array

---
+++ java-array/maximum_Minimum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class maximum_Minimum{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter size of array:");
+        int size=sc.nextInt();
+        int number[]=new int[size];
+
+        for(int i=0 ; i<size ; i++){
+            System.out.print("enter value at index "+i+" :");
+            number[i]=sc.nextInt();
+        }
+
+        for(int i=0 ; i<size ; i++){
+            System.out.println("the value at index "+i+" :"+number[i]);
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i =0 ; i<number.length; i++){
+            if(number[i]>max){
+               max=number[i];
+            }
+
+            if(number[i]<min){
+                min=number[i];
+            }
+        }
+        System.out.println("max:"+max);
+        System.out.println("min:"+min);
+    }
+}
