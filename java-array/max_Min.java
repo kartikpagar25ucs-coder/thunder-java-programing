b118fb882a1139ab4ea46799e0496648a215a630
kartik1208-cyber <pagarkartik2@gmail.com>
2026-09-15 23:49:19 +0530

    write method for minimum and maximum value

---
+++ java-array/max_Min.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class max_Min{
+    public static int max(int arr[]){
+        int max=Integer.MIN_VALUE;
+        for(int i =0 ; i<arr.length ; i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+        
+    }
+
+    public static int min(int arr[]){
+        int min=Integer.MAX_VALUE;
+        for(int i =0 ; i<arr.length; i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter size of array: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for(int i =0 ; i<size ; i++){
+            System.out.print("enter vlaue at index "+i+":");
+            arr[i]=sc.nextInt();
+        }
+
+        int max=max(arr);
+        int min=min(arr);
+        System.out.println("max value in array is:"+max);
+        System.out.println("min value  in array is :"+min);
+    }
+}
